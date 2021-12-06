32fabe2f94e92a2eb42827761839a3db1ce7517f
Paulo Victor Rios <victorios.ba@gmail.com>
2021-12-06 08:12:17 -0300

    improved tests

--- test/count_list_test.exs
+++ test/count_list_test.exs
@@ -2,12 +2,22 @@ defmodule CountListTest do
   use ExUnit.Case
 
   describe "call/1" do
-    test "returns the list count" do
+    test "Count list elements" do
       list = [1, 2, 3]
 
       response = CountList.call(list)
 
       expected_response = 3
+
+      assert response == expected_response
+    end
+
+    test "Passing an empty list" do
+      list = []
+
+      response = CountList.call(list)
+
+      expected_response = 0
 
       assert response == expected_response
     end
